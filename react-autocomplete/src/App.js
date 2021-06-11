5eea0134f1b609baaeee88877228c86723b0b141
PashkevichTania <pashkevichtania2004@gmail.com>
2021-06-11 13:09:12 +0300

    feat: add suggestions

--- react-autocomplete/src/App.js
+++ react-autocomplete/src/App.js
@@ -6,6 +6,7 @@ import React, { useEffect, useState } from 'react';
 function App() {
   const [users, setUsers] = useState([]);
   const [text, setText] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
   useEffect(() => {
     const loadUsers = async () => {
       const response = await axios.get('https://reqres.in/api/users');
@@ -14,6 +15,15 @@ function App() {
     loadUsers();
   }, [])
   const onChangeHandler = (text) => {
+    let matches = []
+    if (text.length > 0) {
+      matches = users.filter(user => {
+        const regex = new RegExp(`${text}`, "gi");
+        return user.email.match(regex)
+      })
+    }
+    console.log('matches', matches)
+    setSuggestions(matches)
     setText(text)
   }
   return (
@@ -22,6 +32,9 @@ function App() {
                onChange={e => onChangeHandler(e.target.value)}
                value={text}
         />
+        {suggestions && suggestions.map((suggestion, i) =>
+            <div key={i} className="suggestion col-md-12 justify-content-md-center">{suggestion.email}</div>
+        )}
       </div>
   );
 }
