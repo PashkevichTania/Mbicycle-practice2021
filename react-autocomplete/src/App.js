0c0e61fe3a23890671a0e654c4070c1f0a08b037
PashkevichTania <pashkevichtania2004@gmail.com>
2021-06-11 12:32:10 +0300

    init: start task

---
+++ react-autocomplete/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+function App() {
+
+  return (
+    <div className="App">
+      <input type="text" />
+    </div>
+  );
+}
+
+export default App;
