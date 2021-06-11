f1abe4203196e076300c5d7557ac9b6d228599f1
PashkevichTania <pashkevichtania2004@gmail.com>
2021-06-11 12:36:07 +0300

    feat: add useState, useEffect

--- react-autocomplete/src/App.js
+++ react-autocomplete/src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 
-function App() {
 
+function App() {
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    const loadUsers = async () => {
+      const response = await axios.get('https://reqres.in/api/users');
+      setUsers(response.data.data)
+    }
+    loadUsers();
+  }, [])
   return (
     <div className="App">
       <input type="text" />
