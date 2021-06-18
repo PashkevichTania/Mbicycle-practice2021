11aac560c51b46e27acaf471e6726683d59627d6
PashkevichTania <pashkevichtania2004@gmail.com>
2021-06-18 12:17:15 +0300

    feat: add interfaces

--- pokemon-app/src/interfaces.ts
+++ pokemon-app/src/interfaces.ts
@@ -2,3 +2,32 @@ export interface IPokemon {
     name: string;
     url: string;
 }
+
+export interface IAbility {
+    ability: {
+        name: string,
+    }
+}
+
+export interface IStat {
+    base_stat: number,
+    stat: {
+        name: string,
+    }
+}
+
+export interface IPokemonDetails {
+    abilities: IAbility[],
+    base_experience: number,
+    height: number,
+    id: number,
+    is_default: boolean,
+    name: string,
+    order: number,
+    species: {
+        name: string,
+        url: string,
+    }
+    stats: IStat[],
+    weight: number,
+}
