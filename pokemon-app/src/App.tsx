12c5c2dc0d60a29a077ed1f7f2a85925a531af6f
PashkevichTania <pashkevichtania2004@gmail.com>
2021-06-21 10:49:55 +0300

    refactor: add navbar style

--- pokemon-app/src/App.tsx
+++ pokemon-app/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './components/Home/home.scss';
 import './components/PokemonPage/pokemonCard.scss';
+import './components/NavBar/nav.scss';
 import Home from "./components/Home/Home";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import PageNotFound from "./components/PageNotFound/PageNotFound";
--- pokemon-app/src/components/NavBar/NavBar.tsx
+++ pokemon-app/src/components/NavBar/NavBar.tsx
@@ -4,7 +4,7 @@ import {NavLink, withRouter} from "react-router-dom";
 const Navbar = () => {
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light" id="nav">
             <div className="navbar-brand">Navigate </div>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
                     aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
